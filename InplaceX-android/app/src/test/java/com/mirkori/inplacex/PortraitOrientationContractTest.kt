f7d7776d6b204971245124233930e1418b294832
coindmit-cmyk <coindmit@gmail.com>
2026-08-11 17:22:32 +0300

    fix(android): lock app to portrait orientation

---
+++ InplaceX-android/app/src/test/java/com/mirkori/inplacex/PortraitOrientationContractTest.kt
@@ -0,0 +1,21 @@
+package com.mirkori.inplacex
+
+import java.io.File
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class PortraitOrientationContractTest {
+    @Test
+    fun mainActivityIsLockedToPortrait() {
+        val manifest = listOf(
+            File("src/main/AndroidManifest.xml"),
+            File("InplaceX-android/app/src/main/AndroidManifest.xml"),
+        ).firstOrNull(File::isFile)?.readText()
+            ?: error("Cannot locate AndroidManifest.xml")
+        val mainActivity = manifest
+            .substringAfter("android:name=\".MainActivity\"")
+            .substringBefore("</activity>")
+
+        assertTrue(mainActivity.contains("android:screenOrientation=\"portrait\""))
+    }
+}
